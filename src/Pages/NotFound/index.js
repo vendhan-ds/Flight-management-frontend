ed114b6db9e21c2ce62c71340973f7690708cede
Aadhithya R P <chahaeinsuho@gmail.com>
2023-10-27 12:51:51 +0530

    added login and register pages

---
+++ src/Pages/NotFound/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './notFound.css';
+
+const NotFound = () => {
+  return (
+    <div className='container'>
+      <h1>Page Not Found</h1>
+      <p>Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been moved to another URL.</p>
+    </div>
+  )
+}
+
+export default NotFound
