f46010223a6648b9d31a61b16f930395ab9bd072
vendhan-ds <104243394+vendhan-ds@users.noreply.github.com>
2023-11-04 01:13:25 +0530

    Login and Regiuster integerated

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
-// import './notFound.css';
+import { Container, Text } from '@mantine/core'
 
 const NotFound = () => {
   return (
-    <div className='container'>
-      <h1>Page Not Found</h1>
-      <p>Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been moved to another URL.</p>
+    <div>
+      <Container>
+        <Text>
+          Not Found
+        </Text>
+      </Container>
     </div>
   )
 }
